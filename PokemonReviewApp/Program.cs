89ca43684fe2dde25f4d5e8ae137e92e451fdfc5
phamduyanminh <phamduyanminh3120@gmail.com>
2023-03-11 00:00:04 -0500

    Seeding the database with seed.cs file and wire up dependencies injection

--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -3,13 +3,15 @@
  */
 
 using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp;
 using PokemonReviewApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+// Wire up dependencies injection. Add the injection at the very beginning when the program runs
+builder.Services.AddTransient<Seed>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
